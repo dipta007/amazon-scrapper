7a51f0368a7d0f9e9e0815e496b47a2eb81f8ca3
Shubhashis Roy Dipta <iamdipta@gmail.com>
2018-08-28 20:51:48 +0600

    Get all the ASIN from in file

--- GetAsin.py
+++ GetAsin.py
@@ -3,7 +3,10 @@ asin_list = []
 
 def get_asins():
     asin_file = open("asin.in");
-    for i in range(100):
-        asin_list.append(asin_file.readline().replace("\n", ""))
+    for line in asin_file:
+        if len(line) > 0:
+            asin_list.append(line)
+    # for i in range(100):
+    #     asin_list.append(asin_file.readline().replace("\n", ""))
     asin_file.close()
     return asin_list
