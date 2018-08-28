65c5f2581228ed469a82e6ecba97460a0fd62f56
dipta007 <iamdipta@gmail.com>
2018-08-28 17:57:51 +0600

    close file connection to avoid memory leak

--- GetAsin.py
+++ GetAsin.py
@@ -1,8 +1,9 @@
-asin_file = open("asin.in");
 asin_list = []
 
 
 def get_asins():
+    asin_file = open("asin.in");
     for i in range(100):
         asin_list.append(asin_file.readline().replace("\n", ""))
+    asin_file.close()
     return asin_list
